4ccade4bd031ab8f2637e78e139c4ff7f2120038
Cam Saul <github@camsaul.com>
2021-08-25 14:46:31 -0700

    Fix column metadata and sync

--- src/metabase/driver/sudoku.clj
+++ src/metabase/driver/sudoku.clj
@@ -1,8 +1,8 @@
 (ns metabase.driver.sudoku
-  (:require [metabase.driver :as driver]
-            [metabase.query-processor.store :as qp.store]
-            [metabase.query-processor.reducible :as qp.reducible]
-            [metabase.driver.sudoku.query-processor :as sudoku.qp]))
+  (:require [clojure.pprint :as pprint]
+            [metabase.driver :as driver]
+            [metabase.driver.sudoku.query-processor :as sudoku.qp]
+            [metabase.query-processor.store :as qp.store]))
 
 (driver/register! :sudoku)
 
@@ -30,22 +30,24 @@
        :schema nil}))})
 
 (defmethod driver/describe-table :sudoku
-  [_driver _Database {table-name :name}]
+  [_driver _database {table-name :name}]
   {:name   table-name
    :schema nil
    :fields (set (for [i (range 1 10)]
-                  {:name          (format "col_%d" i)
-                   :database-type "org.metabase.enterprise_sudoku.NewColumnFactoryAbstractColumnProxyImpl"
-                   :base-type     :type/Integer}))})
+                  {:name              (format "col_%d" i)
+                   :database-type     "org.metabase.enterprise_sudoku.NewColumnFactoryAbstractColumnProxyImpl"
+                   :base-type         :type/Integer
+                   :database-position (dec i)}))})
 
 (defmethod driver/mbql->native :sudoku
   [_driver {{source-table-id :source-table} :query, :as mbql-query}]
-  (println "mbql-query:" mbql-query) ; NOCOMMIT
+  (println "MBQL query:")
+  (pprint/pprint mbql-query)
   (:name (qp.store/table source-table-id)))
 
 (defmethod driver/execute-reducible-query :sudoku
-  [_driver {{difficulty :query} :native, :as native-query} _context respond]
-  (println "native-query:" native-query) ; NOCOMMIT
+  [_driver {difficulty :native, :as query} _context respond]
+  (println "Native query:" (pr-str (select-keys query [:native])))
   (let [metadata (sudoku.qp/column-metadata)
-        rows     (sudoku.qp/rando-board-query-results difficulty)]
+        rows     (sudoku.qp/random-board-rows (keyword difficulty))]
     (respond metadata rows)))
